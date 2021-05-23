2228ad8af5c66798efe8eb8cc21f6926766ec6f4
Joris <Neyens01@gmail.com>
2021-05-23 22:25:54 +0200

    footer links

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -24,17 +24,11 @@ export default function Footer() {
             <Link href="/hoe-het-werkt">
               <a className="d-block">uitleg</a>
             </Link>
-            <Link href="/registreer">
-              <a className="d-block">aanmelden</a>
-            </Link>
           </div>
           <div className="col-12 text-center py-3 py-md-0 text-md-left col-md-2">
             <p className="d-block mb-1">cursus</p>
             <Link href="/cursus-aanbod">
               <a className="d-block">Alle cursussen</a>
-            </Link>
-            <Link href="/">
-              <a className="d-block">Komende cursus</a>
             </Link>
           </div>
           <div className="col-12 col-md-6 d-flex justify-content-center justify-content-md-end p-0">
