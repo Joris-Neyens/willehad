bb9eabc85c1aafc5c582756f14cc2aa4e595764f
Joris <Neyens01@gmail.com>
2021-10-04 21:20:27 +0200

    hamburger menu

--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -2,9 +2,10 @@ import Link from "next/link";
 import { useState } from "react";
 import Fade from "react-reveal/Fade";
 import Hamburger from "hamburger-react";
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+
 
 export default function MainNav() {
 
@@ -15,7 +16,7 @@ export default function MainNav() {
       <Navbar fixed="top" className="menu py-0 d-none d-lg-block">
         <Container className="menu__container">
           <Nav className="me-auto w-100">
-            <nav className="py-2 row w-100">
+            <nav className="py-2 row mx-auto w-100">
               <div className="py-1">
                 <Link href="/">
                   <a id="logo" className="pr-4">
@@ -58,7 +59,7 @@ export default function MainNav() {
           </Nav>
         </Container>
       </Navbar>
-      <Navbar fixed="top" className="menu d-lg-none p-0" expand="lg">
+      {/* <Navbar fixed="top" className="menu d-none p-0" expand="lg">
         <div className="w-100 d-flex justify-content-between px-2">
           <div className="d-flex py-3">
             <Link href="/">
@@ -97,50 +98,65 @@ export default function MainNav() {
             </div>
           </Fade>
         </Navbar.Collapse>
-      </Navbar>
-      <div className="d-flex justify-content-right">
-        <div className="hamburger d-none">
-          <Hamburger
-            laber="show menu"
-            size={30}
-            toggled={isOpen}
-            toggle={setOpen}
-            color="#F6F6F6"
-            onToggle={toggled => {
-              const hamburgerMenu = document.querySelector(".hamburger__menu");
-              if (toggled) {
-                hamburgerMenu.classList.remove("hamburger__menu--close");
-                hamburgerMenu.classList.add("hamburger__menu--open");
-              } else {
-                hamburgerMenu.classList.add("hamburger__menu--close");
-                hamburgerMenu.classList.remove("hamburger__menu--open");
-              }
-            }}
-          />
+      </Navbar> */}
+      <div className="container">
+        <div className="d-flex logo-container position-absolute py-3">
+          <div classname="hamburger-logo">
+            <Link href="/">
+              <a className="pr-4">Willehad</a>
+            </Link>
+          </div>
+        </div>
+        <div className="d-flex justify-content-right">
+          <div className="hamburger py-1 mr-2">
+            <Hamburger
+              laber="show menu"
+              size={30}
+              toggled={isOpen}
+              toggle={setOpen}
+              color="#F6F6F6"
+              onToggle={toggled => {
+                const hamburgerMenu = document.querySelector(".hamburger__menu");
+                if (toggled) {
+                  hamburgerMenu.classList.remove("hamburger__menu--close");
+                  hamburgerMenu.classList.add("hamburger__menu--open");
+                } else {
+                  hamburgerMenu.classList.add("hamburger__menu--close");
+                  hamburgerMenu.classList.remove("hamburger__menu--open");
+                }
+              }}
+            />
+          </div>
         </div>
       </div>
-      <div className="position-absolute hamburger__menu hamburger__menu--open  d-none ">
-        <div className="background-blue py-5 px-5">
-          <Link href="/cursus-aanbod">
-            <a className="text-right menu__link">cursus aanbod</a>
-          </Link>
-          <Link href="/hoe-het-werkt">
-            <a className="text-right menu__link">hoe het werkt</a>
-          </Link>
-          <Link href="/over-Willehad">
-            <a className="text-right menu__link">Over Willehad</a>
-          </Link>
-          <Link href="/contact" className="text-right menu__link">
-            <a className="text-right menu__link">contact</a>
-          </Link>
-          <div className="d-flex justify-content-end pt-4">
-            <Link href="https://willehad.thinkific.com/users/sign_in">
-              <button className="menu__link__dark button__primary px-4">
-                <a className="menu__link--light" title="login">
-                  login
-                </a>
-              </button>
+
+      <div className="position-absolute w-100 hamburger__menu hamburger__menu--close d-lg-none">
+        <div className="hamburger-menu__wrapper w-100 d-flex justify-content-end">
+          <div className="background-blue py-5 px-sm-5 px-4">
+            <Link href="/">
+              <a className="text-right menu__link">Home</a>
             </Link>
+            <Link href="/cursus-aanbod">
+              <a className="text-right menu__link">cursus aanbod</a>
+            </Link>
+            <Link href="/hoe-het-werkt">
+              <a className="text-right menu__link">hoe het werkt</a>
+            </Link>
+            <Link href="/over-Willehad">
+              <a className="text-right menu__link">Over Willehad</a>
+            </Link>
+            <Link href="/contact" className="text-right menu__link">
+              <a className="text-right menu__link">contact</a>
+            </Link>
+            <div className="d-flex justify-content-end pt-4">
+              <Link href="https://willehad.thinkific.com/users/sign_in">
+                <button className="menu__link__dark button__primary px-4">
+                  <a className="menu__link--light" title="login">
+                    login
+                  </a>
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
